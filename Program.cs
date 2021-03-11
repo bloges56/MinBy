84b97ecac7dcffc17040ae85f0ddb45e9cc9a33c
bloges56 <brady.drew.logan@gmail.com>
2021-03-11 16:51:23 -0600

    works but needs to be optimized

--- Program.cs
+++ Program.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MinBy
 {
-    class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static TSource MinBy<TSource, TKey>(
+         this IEnumerable<TSource> source,
+         Func<TSource, TKey> keySelector,
+         IComparer<TKey> comparer = null)
+        {
+            // argument validation and defaulting for comparer
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
+            if (comparer == null) comparer = Comparer<TKey>.Default;
+
+            // TODO: Replace the following bad code with your code
+            // return source.Aggregate((min, x) => comparer.Compare(keySelector(x), keySelector(min)) < 0 ? x : min); // bad, calls keyselector multiple times
+
+            // return source.OrderBy(keySelector, comparer).First(); // bad, sorts entire sequence to find MinBy
+
+            // TKey min = source.Min(keySelector);
+            // return source.First(x => keySelector(x).Equals(min)); // bad, iterates twice, calls keySelector twice, doesn't use comparer
+
+            if(source.First() == null)
+            {
+                return default(TSource);
+            }
+
+            return Helper(source.Skip(1), keySelector, source.First(), keySelector(source.First()), comparer);
+
+            
+        }
+
+        public static TSource Helper<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            TSource min,
+            TKey minKey,
+            IComparer<TKey> comparer = null
+        )
+        {
+            if(!source.Any())
+            {
+                return min;
+            }
+
+            TKey compareValue = keySelector(source.First());
+
+            if(comparer.Compare(compareValue, minKey) < 0)
+            {
+                min = source.First();
+                minKey = compareValue;
+            }
+
+            return Helper(source.Skip(1), keySelector, min, minKey, comparer);
         }
     }
 }
