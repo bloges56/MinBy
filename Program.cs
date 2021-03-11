49aaf9455f9eb3ac5c8b7dd0cae08cbad95559d7
bloges56 <brady.drew.logan@gmail.com>
2021-03-11 14:32:47 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MinBy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
